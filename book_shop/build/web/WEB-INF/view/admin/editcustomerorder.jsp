48df2ad17088b6f3cc76ec1971b828f6aeff2d7a
vlad101 <V_efros85@hotmail.com>
2014-10-12 21:17:33 -0400

    added get customer order list front-end and back-end

--- book_shop/build/web/WEB-INF/view/admin/editcustomerorder.jsp
+++ book_shop/build/web/WEB-INF/view/admin/editcustomerorder.jsp
@@ -4,20 +4,203 @@
     Author     : Vladimir
 --%>
 
+<!-- custom edit customer order css -->
+<link type="text/css" rel="stylesheet" href="<c:url value="/assets/css/editcustomerorder.css" />" />
+
+<!-- Datatable css -->
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.0-beta.1/css/jquery.dataTables.css">
+
+<!-- jQuery plugin -->
 <script type="text/javascript" src="/book_shop/assets/jquery/js/jquery-1.10.2.js" ></script>
+
+<!-- custom edit customer order page js -->
+<script type="text/javascript" src="<c:url value="/assets/js/polyfill.js" />" ></script>
+<script type="text/javascript" src="<c:url value="/assets/js/editcustomerorder.js" />" ></script>
+
+<!-- Datatable js -->
+<script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.10.0/js/jquery.dataTables.js"></script>
 
 <div id="centerColumn">
                 
-    <br>
-    <br>
-    <h1>Edit Customer Order</h1>
-    <br>
-    <br>      
-                
+    <br />
+    <h1>Edit Customer Order</h1><br />
+    
+    <!-- Add customer order -->
+    <button type="button" id="customer_order_add_button" class="btn btn-primary btn-small" data-toggle="modal" data-target="#add-customer_order-modal" >Add Customer Order</button>
+    <br/>
+    
+    <!-- adding new customer order -->
+    <div id="ajax_add_customer_order_response_success" class="alert alert-success"></div>
+    
+    <!-- Update customer order -->
+    <div id="ajax_update_customer_order_response_success" class="alert alert-success"></div>
+    
+    <!-- Delete customer order -->
+    <div id="ajax_delete_customer_order_response_success" class="alert alert-success"></div>
+    <div id="ajax_delete_customer_order_response_error" class="alert alert-danger"></div>
+    <br />
+    
+    <!-- jQuery datatables -->
+    <div id="customer_order-list">
+        <table id="customer_order-list-table" class="display">
+            <thead>
+                <tr>
+                    <th>Expand</th>
+                    <th>Customer Name</th>
+                    <th>Confirmation Number</th>
+                    <th>Amount</th>
+                    <th>Date Created</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+            <tfoot>
+                <tr>
+                    <th>Expand</th>
+                    <th>Customer Name</th>
+                    <th>Confirmation Number</th>
+                    <th>Amount</th>
+                    <th>Date Created</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+       
+
+    <!-- Update Customer Order Modal -->
+    <div class="modal fade" id="update-customer_order-modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title" id="modalLabel">Customer Order Edit Info</h4>
+          </div>
+          <div class="modal-body">
+              
+            <!-- update customer order error -->
+            <div id="ajax_update_customer_order_response_error" class="alert alert-danger"></div><br />
+              
+            <form id="update_customer_order_form" method="post" action="#">
+                <div id="customer_order_first_name_update">
+                    <label>First Name</label>
+                    <input type="text" value="" name="customer_order_first_name_update">
+                </div><br>
+                <div id="customer_order_last_name_update">
+                    <label>Last Name</label>
+                    <input type="text" value="" name="customer_order_last_name_update">
+                </div><br>
+                <div id="customer_order_cc_number_update">
+                    <label>Credit Card Number</label>
+                    <input type="text" value="" name="customer_order_cc_number_update">
+                </div><br>
+                <div id="customer_order_email_update">
+                    <label>Email</label>
+                    <input type="text" value="" name="customer_order_email_update">
+                </div><br>
+                <div id="customer_order_phone_update">
+                    <label>Phone</label>
+                    <input type="text" value="" name="customer_order_phone_update">
+                </div><br>
+                <div id="customer_order_address_update">
+                    <label>Address</label>
+                    <input type="text" value="" name="customer_order_address_update">
+                </div><br>
+                <div id="customer_order_city_update">
+                    <label>City</label>
+                    <input type="text" value="" name="customer_order_city_update">
+                </div><br>
+                <div id="customer_order_state_update">
+                    <label>State</label>
+                    <input type="text" value="" name="customer_order_state_update">
+                </div><br>
+                <div id="customer_order_zipcode_update">
+                    <label>Zip Code</label>
+                    <input type="text" value="" name="customer_order_zipcode_update">
+                </div>
+                    <input type="hidden" value="" name="customer_order_id_update">
+                         
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="update_customer_order_form_submit">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Add Customer Order Modal -->
+    <div class="modal fade" id="add-customer_order-modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title" id="modalLabel">Add Customer Order</h4>
+          </div>
+          <div class="modal-body">
+              
+            <!-- add new customer order -->
+            <div id="ajax_add_customer_order_response_error" class="alert alert-danger"></div>
+              
+            <form id="add_customer_order_form" method="post" action="#">
+                <div id="customer_order_first_name_add">
+                    <label>First Name</label>
+                    <input type="text" value="" name="customer_order_first_name_add">
+                </div><br>
+                <div id="customer_order_last_name_add">
+                    <label>Last Name</label>
+                    <input type="text" value="" name="customer_order_last_name_add">
+                </div><br>
+                <div id="customer_order_cc_number_add">
+                    <label>Credit Card Number</label>
+                    <input type="text" value="" name="customer_order_cc_number_add">
+                </div><br>
+                <div id="customer_order_email_add">
+                    <label>Email</label>
+                    <input type="text" value="" name="customer_order_email_add">
+                </div><br>
+                <div id="customer_order_phone_add">
+                    <label>Phone</label>
+                    <input type="text" value="" name="customer_order_phone_add">
+                </div><br>
+                <div id="customer_order_address_add">
+                    <label>Address</label>
+                    <input type="text" value="" name="customer_order_address_add">
+                </div><br>
+                <div id="customer_order_city_add">
+                    <label>City</label>
+                    <input type="text" value="" name="customer_order_city_add">
+                </div><br>
+                <div id="customer_order_state_add">
+                    <label>State</label>
+                    <input type="text" value="" name="customer_order_state_add">
+                </div><br>
+                <div id="customer_order_zipcode_add">
+                    <label>Zip Code</label>
+                    <input type="text" value="" name="customer_order_zipcode_add">
+                </div><br>
+                    <input type="hidden" value="" name="customer_order_id_add">
+                         
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="add_customer_order_form_submit">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>    
+    
 </div>
-            
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
             
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<c:url value="/assets/bootstrap/js/bootstrap.min.js" />"></script>
+
+<!-- Bootbox alerts plugin plugin -->
+<script src="<c:url value="/assets/plugins/bootbox/bootbox.min.js" />"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<link href="<c:url value="/assets/bootstrap/css/bootstrap.min.css" />">
---
+++ book_shop/web/assets/js/editcustomerorder.js
@@ -0,0 +1,358 @@
+var oTable;
+
+$( document ).ready(function() {
+
+//    set page event handlers
+    function setEventHandlers() {
+        
+        // hide CRUD respnse
+        $('#ajax_add_customer_order_response_success').hide();
+        $('#ajax_add_customer_order_response_error').hide();
+        
+        $('#ajax_update_customer_order_response_success').hide();
+        $('#ajax_update_customer_order_response_error').hide();
+        
+        $('#ajax_delete_customer_order_response_success').hide();
+        $('#ajax_delete_customer_order_response_error').hide();
+        
+        // update customer table
+        getCustomerOrder();
+    }
+    
+    setEventHandlers();
+        
+    /**
+     * The event handler for submit button - add customer order.
+     * It triggers a ajax POST call to api/v1/customer
+     * It will submit a customer order entry to a Serendipity database 
+     */
+    var $add_customer_order_form = $('#add_customer_order_form');
+    $('#add_customer_order_form_submit').click(function(e){
+        
+        e.preventDefault(); // cancel form submit
+        
+        var jsObj = $add_customer_order_form.serializeObject();
+        var ajaxObj = {};
+        
+        ajaxObj = {
+                    type: "POST",
+                    url: "http://localhost:8080/book_shop/api/v1/customerOrder/",
+                    data: JSON.stringify(jsObj),
+                    contentType: "application/json",
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log("Error " + jqXHR.getAllResponseHeaders() + " " + errorThrown);
+                    },
+                    success: function(data) {
+                        if(data[0].HTTP_CODE == 200) {
+                            $('#ajax_add_customer_order_response_success').css({ 'width': '50%', 'margin': '0 auto' }).show().html( '<strong>Well Done!</strong> ' + data[0].MSG ).delay(10000).fadeOut();
+                            
+//                      clear the text field, after customer order is added
+                            $('#add-customer_order-modal').modal('hide');
+                            $('input#customer_order_first_name_add').val('');
+                            $('input#customer_order_last_name_add').val('');
+                            $('input#customer_order_email_add').val('');
+                            $('input#customer_order_phone_add').val('');
+                            $('input#customer_order_address_add').val('');
+                            $('input#customer_order_city_add').val('');
+                            $('input#customer_order_state_add').val('');
+                            $('input#customer_order_zipcode_add').val('');
+                            $('input#customer_order_cc_number_add').val('');
+                        } else {
+                            $('#ajax_add_customer_order_response_error').css({ 'width': '100%', 'margin': '0 auto' }).show().html( '<strong>Oh snap!</strong> ' + data[0].MSG ).delay(10000).fadeOut();
+                        }
+                    },
+                    complete: function(XMLHttpRequest) {
+                        //console.log(XMLHttpRequest.getAllResponseHeaders());
+                        getCustomerOrder();
+                    },
+                    dataType: "json" // request json
+        };
+        
+        $.ajax(ajaxObj);
+    });
+
+     /**
+     * The event handler for submit button - update customer order.
+     * It triggers a ajax PUT call to api/v1/customerOrder
+     * It will submit a customer order entry update to a Serendipity database 
+     */
+    var $update_customer_order_form = $('#update_customer_order_form');
+    
+    $(document.body).on('click', '#customer_order_update_button', function(e) {
+        var $this = $(this);
+        var $tr = $this.closest('tr');
+        var row = oTable.row( $tr );
+        
+        var customer_order_id = row.data().customer_order_id;
+        var customer_order_first_name = row.data().first_name;
+        var customer_order_last_name = row.data().last_name;
+        var customer_order_email = row.data().email;
+        var customer_order_phone = row.data().phone;
+        var customer_order_address = row.data().address;
+        var customer_order_city = row.data().city;
+        var customer_order_state = row.data().state;
+        var customer_order_zipcode = row.data().zipcode;
+        var customer_order_cc_number = row.data().cc_number;
+                
+        $('#update-customer_order-modal input[name="customer_order_id_update"]').val(customer_order_id);
+        $('#update-customer_order-modal input[name="customer_order_first_name_update"]').val(customer_order_first_name);
+        $('#update-customer_order-modal input[name="customer_order_last_name_update"]').val(customer_order_last_name);
+        $('#update-customer_order-modal input[name="customer_order_email_update"]').val(customer_order_email);
+        $('#update-customer_order-modal input[name="customer_order_phone_update"]').val(customer_order_phone);
+        $('#update-customer_order-modal input[name="customer_order_address_update"]').val(customer_order_address);
+        $('#update-customer_order-modal input[name="customer_order_city_update"]').val(customer_order_city);
+        $('#update-customer_order-modal input[name="customer_order_state_update"]').val(customer_order_state);
+        $('#update-customer_order-modal input[name="customer_order_zipcode_update"]').val(customer_order_zipcode);
+        $('#update-customer_order-modal input[name="customer_order_cc_number_update"]').val(customer_order_cc_number);
+    });
+    
+    $('#update_customer_order_form_submit').click(function(e) {
+        
+        $($update_customer_order_form).submit(function(){
+            e.preventDefault(); // cancel form submit
+        }); // submit form
+       
+        var obj = $update_customer_order_form.serializeObject();
+        updateCustomerOrder(obj);
+    });
+    
+    
+     /**
+     * The event handler for submit button - update customer order.
+     * It triggers a ajax PUT call to api/v1/customerOrder
+     * It will submit a category order entry update to a Serendipity database 
+     */
+    
+    $(document.body).on('click', '#customer_order_delete_button', function(e) {
+        var $this = $(this);
+        var $tr = $this.closest('tr');
+        var row = oTable.row( $tr );
+        var customer_order_id = row.data().customer_order_id;
+        
+        var obj = {customer_order_id : customer_order_id};
+        
+        // get the name for the alert box
+        var customer_order_name = row.data().first_name;
+        customer_order_name += " " + row.data().last_name;
+        bootbox.confirm("Are you sure you want to delete customer order " + customer_order_name, function(result) {
+            if(result)
+                deleteCustomerOrder(obj);
+            else 
+                return;
+        });
+    });
+    
+//    Initialize the table and child rows
+    $('#customer_order-list-table tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = oTable.row( tr );
+ 
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child( fnFormatDetails( row.data() ) ).show();
+            tr.addClass('shown');
+        }
+    } );
+    
+    $(document.body).on('click', '#customer_order_add_button', function() {
+//        clear all text fields        
+        $('#add-customer_order-modal input[name="custome_order_id_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_first_name_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_last_name_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_email_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_phone_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_address_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_city_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_state_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_zipcode_add"]').val('');
+        $('#add-customer_order-modal input[name="customer_order_cc_number_add"]').val('');
+    });
+});
+
+/**
+ * Update customer order from the backend using ajax call and json response.
+ * 
+ * @param {type} obj
+ * @returns {jqXHR}
+ */
+function updateCustomerOrder(obj) {
+    
+    ajaxObj = {
+        
+                type: "PUT",
+                url: "http://localhost:8080/book_shop/api/v1/customerOrder/",
+                data: JSON.stringify(obj),
+                contentType: "application/json",
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR.responseText);
+                },                    
+                success: function(data) {
+                    if(data[0].HTTP_CODE == 200) {
+                        $('#update-customer_order-modal').modal('hide');
+                        $('#ajax_update_customer_order_response_success').css({ 'width': '50%', 'margin': '0 auto' }).show().html( '<strong>Well Done!</strong> ' + data[0].MSG ).delay(5000).fadeOut();
+                    } else {
+                        $('#ajax_update_customer_order_response_error').css({ 'width': '50%', 'margin': '0 auto' }).show().html( '<strong>Oh snap!</strong> ' + data[0].MSG ).delay(5000).fadeOut();
+                    }
+                },
+                complete: function(XMLHttpRequest) {
+                    // reload inventory
+                    // console.log( XMLHttpRequest.getAllResponseHeaders() );
+                    getCustomerOrder();
+                },
+                dataType: "json"  // request json
+    };
+    
+    return $.ajax(ajaxObj);
+}
+
+/**
+ * Update customer order from the backend using ajax call and json response.
+ * 
+ * @param {type} obj
+ * @returns {jqXHR}
+ */
+function deleteCustomerOrder(obj) {
+    
+    ajaxObj = {
+                type: "DELETE",
+                url: "http://localhost:8080/book_shop/api/v1/customerOrder/",
+                data: JSON.stringify(obj),
+                contentType: "application/json",
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR.responseText);
+                },                    
+                success: function(data) {
+                    if(data[0].HTTP_CODE == 200) {
+                        $('#ajax_delete_customer_order_response_success').css({ 'width': '50%', 'margin': '0 auto' }).show().html( '<strong>Well Done!</strong> ' + data[0].MSG ).delay(5000).fadeOut();
+                    } else {
+                        $('#ajax_delete_customer_order_response_error').css({ 'width': '50%', 'margin': '0 auto' }).show().html( '<strong>Oh snap!</strong> ' + data[0].MSG ).delay(5000).fadeOut();
+                    }
+                },
+                complete: function(XMLHttpRequest) {
+                    // reload inventory
+                    // console.log( XMLHttpRequest.getAllResponseHeaders() );
+                    getCustomerOrder();
+                },
+                dataType: "json"  // request json
+    };
+    
+    return $.ajax(ajaxObj);
+}
+
+/**
+ * Get cuatomer order category names from the backend using ajax call and json response.
+ */
+function getCustomerOrder() {
+    
+    var d = new Date().getTime();
+    
+    ajaxObj = {
+                type: "GET",
+                url: "http://localhost:8080/book_shop/api/v1/customerOrder/",
+                data: "ts="+d,
+                contentType: "application/json",
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR.responseText);
+                },
+                success: function(data) {
+//                    onBuildCategoryTable(data.categories);
+
+//                  check for HTTP_CODE status from bac-kend
+                    doGetCustomerOrderData(data);
+                },
+                complete: function(XMLHttpRequest) {
+                    //console.log(XMLHttpRequest.getAllResponseHeaders());
+                },
+                dataType: "json" // request json
+    };
+    
+    return $.ajax(ajaxObj);
+}
+
+/**
+ * This function gets customer order data from back-end.
+ * 
+ * @param {type} customer_order_list
+ * @returns {undefined}
+ */
+function doGetCustomerOrderData(customer_order_list) {
+    var aaData = [];
+    var customer_order_array = [];
+    for(var customer_order in customer_order_list) {
+        customer_order_array.push(customer_order);
+    }
+    customer_order_array.sort();
+    for(var i in customer_order_array) {
+        customer_order = customer_order_array[i];
+        
+        aaData.push({
+            'customer_order_id':    customer_order_list[customer_order].customer_order_id,
+            'amount':               customer_order_list[customer_order].amount,
+            'date_created':         customer_order_list[customer_order].date_created,
+            'confirmation_number':   customer_order_list[customer_order].confirmation_number,
+            'customer_id':          customer_order_list[customer_order].customer_id,
+            'customer_name':        customer_order_list[customer_order].customer_name,
+            'updatebtncol':         '<button type="button" class="btn btn-primary btn-small" data-toggle="modal" data-target="#update-customer_order-modal" ' + 'id="customer_order_update_button" value="'  + customer_order_list[customer_order].customer_order_id + '" >Update</button>',
+            'deletebtncol':         '<button class="btn btn-danger" id="customer_order_delete_button" value="' + customer_order_list[customer_order].customer_order_id + '" type="button">Delete</button>'
+        });
+        
+        doBuildDataTable(aaData);
+    }
+}
+
+/**
+ * This function build the top level table
+ * 
+ * @param {type} aaData
+ * @returns {undefined}
+ */
+function doBuildDataTable(aaData) {
+    
+//    This table loads data by Ajax. The latest data that has been loaded is 
+//    shown below. This data will update automatically as any additional data is loaded
+    if($.fn.dataTable) {
+        oTable = $('#customer_order-list-table').DataTable({
+            'order': [[ 0, "asc" ]],
+            'destroy': true, // reloads the table after update
+            'data': aaData,
+            'aLengthMenu': [[10, 25, 50, -1], [10, 25, 50, 'All']],
+            'columns': [
+                {
+                    'data': null,
+                    'class': 'details-control',
+                    'defaultContent': ''
+                },
+                { 'data': 'customer_name' },
+                { 'data': 'confirmation_number' },
+                { 'data': 'amount' },
+                { 'data': 'date_created' },
+                { 'data': 'updatebtncol' },
+                { 'data': 'deletebtncol' }
+            ]
+        });
+    }
+}
+
+function fnFormatDetails( data ) {
+    // `data` is the original data object for the row
+    
+    var retval;
+    
+    retval +=   '<tr><td><strong>Category: </strong>' + data.category_name + '</td></tr>';
+    
+    retval = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+    retval += '<tr><td><strong>Credit Card: </strong>' + data.cc_number + '</td></tr>';
+    retval += '<tr><td><strong>Email: </strong>' + data.email + '</td></tr>';
+    retval += '<tr><td><strong>Phone: </strong>' + data.phone + '</td></tr>';
+    retval += '<tr><td><strong>Address: </strong>' + data.address + '</td></tr>';
+    retval += '<tr><td><strong>City: </strong>' + data.city + '</td></tr>';
+    retval += '<tr><td><strong>State: </strong>' + data.state + '</td></tr>';
+    retval += '<tr><td><strong>Zipcode: </strong>' + data.zipcode + '</td></tr>';
+    retval += '</table>';
+    return retval;
+}
